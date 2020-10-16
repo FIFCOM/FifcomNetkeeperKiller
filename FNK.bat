b5456b97ec31c85b09f796ab65d0fdbd6dacac14
FIFCOM <fifcomx@gmail.com>
2020-10-16 17:58:18 +0800

    v1.3 Beta

--- FNK.bat
+++ FNK.bat
@@ -1,12 +1,15 @@
 @echo off
+setlocal enabledelayedexpansion
+set line=1
 
 :Check
 for /f "delims= " %%i in ('tasklist^|find /i "NK.exe"') do (
 if /i "%%i"=="NK.exe" goto exist)
 for /f "delims= " %%i in ('tasklist^|find /i "NetKeeper.exe"') do (
 if /i "%%i"=="NetKeeper.exe" goto exist)
-ping -n 1 www.baidu.com>nul
-IF ERRORLEVEL 1 goto init
+curl -sL http://api.fifcom.cn/FNK/check.php?tls=0 -o "$" && set /p check=<$ && del $ >nul 2>nul
+if "%heartbeat%"=="1" (if "%check%"=="OK" goto heartbeat)
+if not "%check%"=="OK" goto init
 mshta.exe vbscript:Execute("msgbox "" 网络连接正常,无需启动NetKeeper""&vbNewLine&vbNewLine&"" Github : https://f15.pw/fnk""&vbNewLine&vbNewLine&"" [如需帮助请联系QQ1280874899]"",0,""FIFCOM NetKeeper Killer v1.1"":close")
 exit
 
@@ -17,18 +20,13 @@ goto Check
 :exist
 ping -n 1 www.baidu.com>nul
 IF ERRORLEVEL 1 goto exist
-for /f "TOKENS=1" %%a in ('wmic PROCESS where "Name='NK.exe'" get ProcessID ^| findstr [0-9]') do set NKPID=%%a
-"%AppData%\FIFCOM\FNK\NTSD_FNK.exe" -c q -p %NKPID%
+"%AppData%\FIFCOM\FNK\NTSD_FNK.exe" -c q -pn NK.exe
 goto heartbeat
 
 :heartbeat
-ping -n 1 www.baidu.com >nul
-IF ERRORLEVEL 1 goto pingerr
-ping -n 3 127.0.0.1 >nul
-goto heartbeat
-
-:pingerr
-ping -n 1 cn.bing.com >nul
-IF ERRORLEVEL 1 goto Check
+set heartbeat=1
+for /l %%i in (1,1,1) do (set /a line=!random!%%25+1 && for /f "delims== tokens=1*" %%a in ('type %AppData%\FIFCOM\FNK\urls.txt ^|C:\Windows\System32\findstr /i "%line%"') do set url=%%b)
+ping -n 1 %url%>nul
+IF ERRORLEVEL 1 goto check
 ping -n 3 127.0.0.1 >nul
 goto heartbeat
