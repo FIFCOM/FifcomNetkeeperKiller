ab465a84b7b9fa68ffc2a4471840fa24a81d4542
FIFCOM <fifcomx@gmail.com>
2020-10-17 17:09:01 +0800

    Update FNK_Uninstaller.bat

--- FNK_Uninstaller.bat
+++ FNK_Uninstaller.bat
@@ -4,7 +4,9 @@ color 8b
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&& echo. && echo      未获取管理员权限 && ping -n 3 127.0.0.1>nul && exit
 del "%appdata%\FIFCOM\FNK\FNK.exe" >nul 2>nul
 del "%appdata%\FIFCOM\FNK\NTSD_FNK.exe" >nul 2>nul
+del "%appdata%\FIFCOM\FNK\urls.txt" >nul 2>nul
 del "C:\Users\%username%\Desktop\NetKeeper_FNK.lnk"
+rd "%appdata%\FIFCOM\FNK\logs" >nul 2>nul
 rd "%appdata%\FIFCOM\FNK" >nul 2>nul
 cls
 echo.
